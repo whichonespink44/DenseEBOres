e2bcee5086d12b9dc9ca174de6028804f99bc182
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-02-16 21:25:45 +0000

    Initial commit (again)

---
+++ src/main/java/com/hokecraft/debo/Debo.java
@@ -0,0 +1,30 @@
+package com.hokecraft.debo;
+
+import com.hokecraft.debo.reference.ModInfo;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION)
+public class Debo
+{
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/hokecraft/debo/reference/ModInfo.java
@@ -0,0 +1,8 @@
+package com.hokecraft.debo.reference;
+
+public class ModInfo
+{
+    public static final String MOD_ID = "debo";
+    public static final String MOD_NAME = "Dense EB Ores";
+    public static final String MOD_VERSION = "1.0";
+}
